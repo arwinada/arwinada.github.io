bbc2a5a086d8178d5f3f1eeb48d8843a98708bf6
arwinada <arwin.chan@ada.support>
2025-05-05 16:14:56 -0400

    added ngrok URL

--- convo_api_chat/script.js
+++ convo_api_chat/script.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async (e) => {
   input.value = '';
 
   // Send POST to backend
-  const response = await fetch('https://your-api-endpoint.com/send', {
+  const response = await fetch('https://2004-2a09-bac5-1709-123-00-1d-d9.ngrok-free.app', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
